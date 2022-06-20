8c27b84f68f00874d762c22c0baa7c4ef943edc2
Furqan <furqan.cloud.dev@gmail.com>
2022-06-20 17:37:21 +0500

    created Xcode 13 project - minimum target iOS 15.0

---
+++ Stocks/Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Furqan on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
